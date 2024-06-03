28dd6b491e6bba683d0f5b395babf5539a07fe3c
ymshin80 <siyumy80@gmail.com>
2024-06-03 14:01:01 +0900

    node kafka 테스트

--- app.js
+++ app.js
@@ -5,12 +5,63 @@ const cors = require("cors")
 const WebSocket = require("ws");
 const { SocketLogger } = require( './logs/winston' );
 const { NewRoom } = require( './types/Room' );
-
+const  cron  = require("node-cron")
 
 
 const server = http.createServer();
 const wss = new WebSocket.Server({server});
 const room = NewRoom()
+const { Kafka } = require("kafkajs");
+
+
+
+const kafka = new Kafka({
+    clientId : 'chat-poducer-1',
+    brokers: ['localhost:19092']
+})
+const topic = "test-topic"
+
+const producer = kafka.producer();
+const consumer = kafka.consumer({ groupId: 'test-group' });
+
+cron.schedule('*/5 * * * * *', async () => {
+    try {
+        await producer.connect()
+
+        await producer.send({
+            topic,
+            messages:[
+                {value: '10.112.58.92:1010'}
+            ]
+        })
+
+    } catch (error) {
+        console.error("Error Sending message from cron job",error)
+    }
+})
+
+
+
+getMessage = async () => {
+    try {
+        await consumer.connect()
+        await consumer.subscribe({
+            topic,
+            fromBeginning: true
+        })
+        
+        await consumer.run({
+            eachMessage: async ({topic, partition, message}) => {
+                const prefix = `${topic}[${partition} | ${message.offset}] / ${message.timestamp}`
+                console.log(`- ${prefix} ${message.key}#${message.value}`)
+            }
+        })  
+    } catch (error) {
+        console.error("Error Sending message from cron job",error)
+    }
+}
+
+getMessage()
 
 wss.on("connection", (ws, req) => {
     //사용자 정보 가져오기
